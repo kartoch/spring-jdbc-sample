5b26096dd789bed4b21307cf72cd2b1e5c6b4b76
jcartign <julien.cartigny@polytech-lille.fr>
2016-09-15 11:50:47 +0200

    Switch to @SpringBootTest as @SpringApplicationConfiguration is deprecated

--- src/test/java/fr/plil/sio/persistence/jdbc/GroupServiceTest.java
+++ src/test/java/fr/plil/sio/persistence/jdbc/GroupServiceTest.java
@@ -5,13 +5,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = JdbcApplication.class)
+@SpringBootTest(classes = JdbcApplication.class)
 public class GroupServiceTest extends AbstractServiceSupport {
 
     @Autowired
